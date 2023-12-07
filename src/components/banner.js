198c3a69747079e1f0c9f5d01e7dfb98d70cf775
GenteKrasniqi <gk58713@ubt-uni.net>
2023-12-07 21:35:59 +0100

    Banner Component Changes

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,5 +1,7 @@
 import '../../src/index.css';
 import Button from '../components/button';
+import '../../src/index.css'
+import { Link } from 'react-router-dom';
 
 const Banner = ({ img, h3, altAttr, h6, title }) => {
     return (
@@ -10,7 +12,10 @@ const Banner = ({ img, h3, altAttr, h6, title }) => {
             <div className="txtHolder">
                 <h3>{h3}</h3>
                 <h6>{h6}</h6>
-                <Button title={title}/>
+                <Link to={'/products'}>
+                    <Button title={title} />
+                </Link>
+
             </div>
         </div>
     )
