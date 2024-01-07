906356b5a0adbf9c62d56cf68d0762ca421157e6
GenteKrasniqi <gk58713@ubt-uni.net>
2024-01-07 23:08:46 +0100

    Http Requests

--- src/Http/Requests.js
+++ src/Http/Requests.js
@@ -162,6 +162,135 @@ const deleteSubs = async (id) => {
   }
 };
 
+const postProductReq = async (data) => {
+  try {
+      const response = await fetch(`https://localhost:7282/api/Products`, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',       
+          },
+          body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      return result;
+  } catch (error) {
+      console.error('Error during fetch:', error);
+      throw error;
+  }
+};
+
+const getProductReq = async () => {
+  try {
+    const response = await fetch(`https://localhost:7282/api/Products`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error('Error during fetch:', error);
+    throw error;
+  }
+};
+
+const getProductById = async (id) => {
+  try {
+      const response = await fetch(`https://localhost:7282/api/Products/${id}`);
+      if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const result = await response.json();
+      return result;
+  } catch (error) {
+      console.error('Error fetching message by ID:', error);
+      throw error;
+  }
+};
+
+const deleteProductReq = async (id) => {
+  try {
+    const apiUrl = `https://localhost:7282/api/Products/${id}`;
+
+    const response = await fetch(apiUrl, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id }), 
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error('Error during fetch:', error);
+    throw error;
+  }
+};
+
+
+const updateProduct = async (id, data) => {
+  try {
+    const response = await fetch(`https://localhost:7282/api/Products/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    // Check if the response body is empty
+    const result = response.status === 204 ? null : await response.json();
+
+    return result;
+  } catch (error) {
+    console.error('Error during fetch:', error);
+    throw error;
+  }
+};
+
+
+const login = async (email,password) => {
+  try {
+    const response = await fetch(`https://localhost:7282/api/Users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(email,password),
+    });
+
+    if (!response.ok) {
+      const errorMessage = await response.text();
+      throw new Error(`HTTP error! Status: ${response.status}, Message: ${errorMessage}`);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error('Error during fetch:', error);
+    throw error;
+  }
+};
+
 export {
     postContactForm ,
     getContactMsg,
@@ -170,5 +299,11 @@ export {
     postSubsInput,
     getSubs,
     getSubscriberById,
-    deleteSubs
+    deleteSubs, 
+    postProductReq,
+    getProductReq,
+    getProductById,
+    deleteProductReq,
+    updateProduct,
+    login
 };
